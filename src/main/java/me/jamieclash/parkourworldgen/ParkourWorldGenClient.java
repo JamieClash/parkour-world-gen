31fe16c8717456a2867efdee461af33ec51de836
Jamie Yeung <jamiethy@gmail.com>
2026-01-05 16:00:12 +0000

    Initialization

---
+++ src/main/java/me/jamieclash/parkourworldgen/ParkourWorldGenClient.java
@@ -0,0 +1,11 @@
+package me.jamieclash.parkourworldgen;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ParkourWorldGenClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
